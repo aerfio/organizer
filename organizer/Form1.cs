14e0dceafb21b46c85738b51c67cb72b96062c85
Aerfio <Aerfio@DESKTOP-UIDJGND>
2017-07-22 00:36:06 +0200

    Add project files.

---
+++ organizer/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace organizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            tabControl1.ItemSize = new Size(tabControl1.Width / tabControl1.TabCount - 3, 0);
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+           tabControl1.ItemSize = new Size(tabControl1.Width / tabControl1.TabCount -3, 0);
+            
+        }
+    }
+}
